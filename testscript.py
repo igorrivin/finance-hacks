e2b2474dde4ac13f22a8a6cab92d7d7767c45bc3
Igor Rivin <igor@edgestreamlp.com>
2020-02-14 12:57:25 -0500

    the tester

--- testscript.py
+++ testscript.py
@@ -2,10 +2,6 @@ from spectral import *
 import pandas as pd
 import numpy as np
 import scipy
-
-import matplotlib.pyplot as plt
-%matplotlib inline
-
 import seaborn as sns
 
 from sklearn.metrics import r2_score
@@ -14,6 +10,10 @@ import os
 from datetime import datetime
 import pandas_datareader.data as web
 from pymssa2 import MSSA
+
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="1,2"
 
 spy = web.DataReader("SPY", "av-daily-adjusted", start=datetime(2000, 2, 9), api_key=os.getenv('ALPHAVANTAGE_API_KEY')).close.map(np.log)
 iwm = web.DataReader("IWM", "av-daily-adjusted", start=datetime(2000, 2, 9), api_key=os.getenv('ALPHAVANTAGE_API_KEY')).close.map(np.log)
@@ -24,6 +24,6 @@ mydf = pd.concat([spy, iwm, dia, qqq], axis=1)
 mydf.index=pd.to_datetime(mydf.index)
 mydf.columns = ['spy', 'iwm', 'dia', 'qqq']
 mydf.dropna(inplace=True)
-foor = dorange(mydf, ['spy', 'iwm', 'dia', 'qqq'], 10, 0, 1000, 1000)
+foor = dorange(mydf, ['spy', 'iwm', 'dia', 'qqq'], 10, 0, 1000, 10)
 foordf = pd.DataFrame(foor)
 foordf.to_csv('foordf.csv')
